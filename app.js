aa608ee1bad42040a5e5ca0e828a880f84b32631
attachaudhury@gmail.com <attachaudhury@gmail.com>
2022-02-03 16:11:05 +0500

    make it better

--- app.js
+++ app.js
@@ -35,7 +35,10 @@ app.use('/search', searchRoute);
 // http.createServer(app).listen(3000, () => {
 //   console.log('Server started on port 3000');
 // });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-    dbSeedService.seedDb();
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+//     dbSeedService.seedDb();
+// });
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
